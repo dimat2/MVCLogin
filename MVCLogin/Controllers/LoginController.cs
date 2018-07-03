fd8f0aee51f7f7afa1043be78f62bff7af9b430d
Kiss Péter <kiss.peti86@gmail.com>
2018-07-04 00:51:52 +0200

    Add project files.

---
+++ MVCLogin/Controllers/LoginController.cs
@@ -0,0 +1,45 @@
+﻿using MVCLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(User userModel)
+        {
+            using (LoginDBEntities db = new LoginDBEntities())
+            {
+                var userDetails = db.User.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "A belépési adatok nem jók.";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.UserID;
+                    Session["userName"] = userDetails.UserName;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            int userId = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
